ea31c4eb0ba128aba12a269584d142f711ef812c
Dinesh Karthik <dineshkarthik0218@gmail.com>
2026-03-18 19:44:43 +0530

    added requirements file

--- jobportal_new/settings.py
+++ jobportal_new/settings.py
@@ -127,3 +127,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
